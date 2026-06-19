d8616c873a34ac62a67f4e2b38a7d209741dd0be
Soysakiaytuno <leonardhopst@gmail.com>
2026-06-19 16:37:24 -0400

    supabase conexion correccion para IPV4

--- Bakcend/HotelBackend/obj/Debug/net10.0/HotelBackend.AssemblyInfo.cs
+++ Bakcend/HotelBackend/obj/Debug/net10.0/HotelBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e72972c79770dddec50139bae3c651e6e3feddbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2020788e7cbeea21730860fcb5ee67a297692bb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
