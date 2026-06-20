3c9d7b6c392aa471e3c184424f64ab2725a36797
Soysakiaytuno <leonardhopst@gmail.com>
2026-06-20 08:06:03 -0400

    feat: [HU-06] implementar validacion de fechas en cobro para pasar test

--- Bakcend/HotelBackend/Models/ModuloEstadias/Cobro.cs
+++ Bakcend/HotelBackend/Models/ModuloEstadias/Cobro.cs
@@ -8,6 +8,10 @@ public class Cobro: CobroBase
         {
             throw new Exception("No se han registrado las fechas de Check-In y Check-Out.");
         }
+        if(estadia.FechaCheckOutReal.Value.Date < estadia.FechaCheckInReal.Value.Date)
+        {
+            throw new ArgumentException("La fecha de Check-Out real no puede ser anterior a la de Check-In real.");
+        }
         var diferencia = (estadia.FechaCheckOutReal.Value.Date - estadia.FechaCheckInReal.Value.Date).Days;
         int DiasCobrados = diferencia < 1 ? 1 : diferencia;
         decimal MontoTotal = DiasCobrados * precioTotal;
